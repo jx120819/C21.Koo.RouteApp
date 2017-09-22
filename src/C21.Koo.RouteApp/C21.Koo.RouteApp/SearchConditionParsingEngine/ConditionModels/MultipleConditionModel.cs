ff81b6226d372aea71fa4b31e7944a69bd72f9d5
张剑 <15028126013@163.com>
2017-09-22 10:47:09 +0800

    修复bug

--- src/C21.Koo.RouteApp/C21.Koo.RouteApp/SearchConditionParsingEngine/ConditionModels/MultipleConditionModel.cs
+++ src/C21.Koo.RouteApp/C21.Koo.RouteApp/SearchConditionParsingEngine/ConditionModels/MultipleConditionModel.cs
@@ -5,30 +5,17 @@ using System.Web;
 
 namespace C21.Koo.RouteApp.SearchConditionParsingEngine
 {
-    public class MultipleConditionModel
+    public class MultipleConditionModel : List<string>
     {
-        private List<string> _container = new List<string>();
-
         /// <summary>
         /// 添加选项
         /// </summary>
         /// <param name="tagId"></param>
-        public void Add(string tagId)
+        public new void Add(string tagId)
         {
-            if (!_container.Contains(tagId))
+            if (!base.Contains(tagId))
             {
-                _container.Add(tagId);
-            }
-        }
-
-        /// <summary>
-        /// 获取所有选项
-        /// </summary>
-        public List<string> Options
-        {
-            get
-            {
-                return _container;
+                base.Add(tagId);
             }
         }
     }
